c3dc35e9d4132dfcb7ec0732eee9e3074845ffbf
chetat <yekuwilfred@gmail.com>
2020-06-24 22:25:52 +0100

    re-write navbar with bootstrap

--- src/Containers/Navbar/index.js
+++ src/Containers/Navbar/index.js
@@ -1,144 +1,34 @@
 import React, { useState } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AddIcon from '@material-ui/icons/Add';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import useStyles from './styles';
-import { Link } from 'react-router-dom';
+import {
+    Button,
+    NavDropdown,
+    Container,
+    Navbar, Nav,
+    Form, FormControl
+} from 'react-bootstrap';
 
 
-const PrimarySearchAppBar = () => {
-    const classes = useStyles();
-    const [anchorEl, setAnchorEl] = useState(null);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
-
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-    const handleMobileMenuClose = () => {
-        setMobileMoreAnchorEl(null);
-    };
-
-    const handleMenuClose = () => {
-        setAnchorEl(null);
-        handleMobileMenuClose();
-    };
-
-    const handleMobileMenuOpen = (event) => {
-        setMobileMoreAnchorEl(event.currentTarget);
-    };
-
-    const menuId = 'primary-search-account-menu';
-    const renderMenu = (
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            id={menuId}
-            keepMounted
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            open={isMenuOpen}
-            onClose={handleMenuClose}
-        >
-            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-        </Menu>
-    );
-
-    const mobileMenuId = 'primary-search-account-menu-mobile';
-    const renderMobileMenu = (
-        <Menu
-            anchorEl={mobileMoreAnchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            id={mobileMenuId}
-            keepMounted
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            open={isMobileMenuOpen}
-            onClose={handleMobileMenuClose}>
-            <MenuItem>
-                <AddIcon />
-                <Typography noWrap>
-                    <Link to="/events/new">
-                        Create Event
-                        </Link>
-                </Typography>
-            </MenuItem>
-            <MenuItem>
-                <AccountCircle />
-                <Typography noWrap>
-                    <Link to="/events/all">
-                        Browse Events
-                        </Link>
-                </Typography>
-            </MenuItem>
-            <MenuItem>
-                <AccountCircle />
-                <Typography noWrap>
-                    <Link to="/auth/login">
-                        Sign In
-                        </Link>
-                </Typography>
-            </MenuItem>
-        </Menu>
-    );
+const AppBar = () => {
     return (
-        <div className={classes.grow}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography className={classes.title} variant="h6" noWrap>
-                        Eveno
-                    </Typography>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
-                        </div>
-                        <InputBase
-                            placeholder="Search Events…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{ 'aria-label': 'search' }}
-                        />
-                    </div>
-                    <div className={classes.grow} />
-                    <div className={classes.sectionDesktop}>
-                            <Link to="/events/all" className={classes.link}>
-                                Browse Events
-                                </Link>
-                    </div>
-                    <div className={classes.sectionDesktop}>
-                            <Link to="/manage/events/new" className={classes.link}>
-                                Create Event
-                                </Link>
-                    </div>
-                    <div className={classes.sectionDesktop}>
-                            <Link to="/auth/login" className={classes.link}>
-                                Sign In
-                                </Link>
-                    </div>
-                    <div className={classes.sectionMobile}>
-                        <IconButton
-                            aria-label="show more"
-                            aria-controls={mobileMenuId}
-                            aria-haspopup="true"
-                            onClick={handleMobileMenuOpen}
-                            color="inherit"
-                        >
-                            <MoreIcon />
-                        </IconButton>
-                    </div>
-                </Toolbar>
-            </AppBar>
-            {renderMobileMenu}
-            {renderMenu}
-        </div>
-    );
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="/">Eveno</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mx-auto">
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-primary">Search</Button>
+                    </Form>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="/manage/events/new">Create Event</Nav.Link>
+                    <Nav.Link eventKey={2} href="/auth/login">
+                       Login
+                    </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
 }
-export default PrimarySearchAppBar;
+
+export default AppBar;
